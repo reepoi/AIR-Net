b1ebdf2c41162f00f4e0df090907a07cc9a10b9c
Taos Transue <spacechips@rocketmail.com>
2023-01-09 16:56:48 +0000

    update plots

--- dmf-vector-fields/plots.py
+++ dmf-vector-fields/plots.py
@@ -1,3 +1,4 @@
+import itertools
 from pathlib import Path
 import io
 
@@ -6,6 +7,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import train
+
+
+FONT_SIZE = 14
 
 
 def matplotlib_to_PIL_Image(fig):
@@ -56,13 +60,17 @@ def plot_report_data(ax, data_set, time, algorithm, technique, grid_density, com
     ax.scatter(nmae['mask_rate'], nmae['nmae'], label=label)
 
 
-def plot_nmae(ax, data_set, algorithm, technique, component, num_factors=1):
+def plot_nmae(ax, data_set, algorithm, technique, num_factors=1, grid_density=100):
     algorithm_nmae_files = (OUTPUT_DIR / data_set.value / algorithm.value).rglob('nmae.json')
     if algorithm is train.Algorithm.DMF:
         algorithm_nmae_files = [p for p in algorithm_nmae_files if f'num_factors{num_factors}' in p.parts]
+    if technique is train.Technique.INTERPOLATED:
+        algorithm_nmae_files = [p for p in algorithm_nmae_files if f'grid_density{grid_density}' in p.parts]
     technique_nmae_files = [p for p in algorithm_nmae_files if technique.value in p.parts]
     files = sorted(technique_nmae_files)
-    assert len(files) != 0, 'No files found!'
+    if len(files) == 0:
+        print(f'No files found! {data_set.value}, {algorithm.value}, {technique.value}, {num_factors}, {grid_density}')
+        return
     nmae_jsons = [pd.read_json(p, typ='series') for p in files]
     mask_rates = [0.3, 0.5, 0.7, 0.9]
     for j, p in zip(nmae_jsons, files):
@@ -71,41 +79,42 @@ def plot_nmae(ax, data_set, algorithm, technique, component, num_factors=1):
     nmae = pd.concat(nmae_jsons)
 
     ax.set_xticks(mask_rates)
-    ax.set_xlabel('Mask Rate')
-    ax.set_ylabel('NMAE')
+    ax.tick_params(axis='both', labelsize=FONT_SIZE)
+    ax.set_xlabel('Mask Rate', fontsize=FONT_SIZE)
+    ax.set_ylabel('log(NMAE)', fontsize=FONT_SIZE)
     label = algorithm.value
     if algorithm is train.Algorithm.DMF:
         label = f'{algorithm.value}_{num_factors}'
-    ax.scatter(nmae['mask_rate'], nmae[component], label=label)
+    import pdb
+    pdb.set_trace()
+    ax.plot(
+        nmae['mask_rate'], sum(np.log(nmae[c].to_numpy()) for c in ['velx_by_time', 'vely_by_time']),
+        marker='o', linestyle='dashed',
+        linewidth=1, markersize=7,
+        label=label
+    )
 
 
 if __name__ == '__main__':
     OUTPUT_DIR = Path(__file__).parent / '..' / 'out' / 'output'
+    image_format = 'pdf'
 
-    data_set = train.DataSet.FUNC1
+    for t, d in itertools.product(train.Technique, [train.DataSet.ANEURYSM, train.DataSet.DOUBLE_GYRE]):
+        fig, ax = plt.subplots()
+        plot_nmae(ax, d, train.Algorithm.IST, t)
+        for nf in [2, 3, 4, 5]:
+            plot_nmae(ax, d, train.Algorithm.DMF, t, num_factors=nf)
+        plot_name = f'figures/{d.value}_{t.value}.{image_format}'
+        ax.legend(fontsize=FONT_SIZE)
+        fig.savefig(plot_name, format=image_format)
+        plt.close(fig)
 
-    technique = train.Technique.IDENTITY
-
-    # fig, axes = plt.subplots(nrows=2, ncols=3)
-    # for a in np.nditer(axes, flags=['refs_ok']):
-    #     a.item().set_xticks(mask_rates)
-
-    fig, ax = plt.subplots()
-    # component = 'velx_by_time'
-    # plot_nmae(ax, data_set, train.Algorithm.IST, technique, component)
-    # plot_nmae(ax, data_set, train.Algorithm.DMF, technique, component, num_factors=2)
-    # plot_nmae(ax, data_set, train.Algorithm.DMF, technique, component, num_factors=3)
-    # plot_nmae(ax, data_set, train.Algorithm.DMF, technique, component, num_factors=4)
-    # plot_nmae(ax, data_set, train.Algorithm.DMF, technique, component, num_factors=5)
-    component = 'velx'
-    time = 0
-    grid_density = 100
-    technique = train.Technique.INTERPOLATED
-    plot_report_data(ax, data_set, time, train.Algorithm.IST, technique, grid_density, component)
-    plot_report_data(ax, data_set, time, train.Algorithm.DMF, technique, grid_density, component, num_factors=2)
-    plot_report_data(ax, data_set, time, train.Algorithm.DMF, technique, grid_density, component, num_factors=3)
-    # plot_report_data(ax, data_set, time, train.Algorithm.DMF, technique, grid_density, component, num_factors=4)
-    # plot_report_data(ax, data_set, time, train.Algorithm.DMF, technique, grid_density, component, num_factors=5)
-    ax.legend()
-    fig.savefig(f'figures/{data_set.value}_{technique.value}_timeframe_{component}.png')
-    plt.close(fig)
+    # c = 'velx'
+    # time = 0
+    # grid_density = 100
+    # plot_report_data(ax, data_set, time, train.Algorithm.IST, t, grid_density)
+    # plot_report_data(ax, data_set, time, train.Algorithm.DMF, t, grid_density, num_factors=2)
+    # plot_report_data(ax, data_set, time, train.Algorithm.DMF, t, grid_density, num_factors=3)
+    # plot_report_data(ax, data_set, time, train.Algorithm.DMF, t, grid_density, num_factors=4)
+    # plot_report_data(ax, data_set, time, train.Algorithm.DMF, t, grid_density, num_factors=5)
+    # plot_name = f'figures/{data_set.value}_{t.value}_timeframe_{c}.png'
